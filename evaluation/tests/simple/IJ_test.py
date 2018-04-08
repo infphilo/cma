dcc7fa232890c41f1cd552a3c0c91caa2fd23029
Daehwan Kim <infphilo@gmail.com>
2018-04-08 11:08:20 -0500

    .

--- evaluation/tests/simple/IJ_test.py
+++ evaluation/tests/simple/IJ_test.py
@@ -1,11 +1,24 @@
-from ij import IJ, ImagePlus, ImageStack  
+from ij import IJ, ImagePlus, ImageStack
+from script.imglib.analysis import DoGPeaks  
+from script.imglib.color import Red  
+from script.imglib.algorithm import Scale2D  
+from script.imglib.math import Compute  
+from script.imglib import ImgLib  
+from ij3d import Image3DUniverse  
+# from javax.vecmath import Color3f, Point3f  
   
 # Load a stack of images: a fly brain, in RGB  
 imp = IJ.openImage("http://imagej.nih.gov/ij/images/flybrain.zip")  
-stack = imp.getImageStack()  
+stack = imp.getImageStack()
   
 print "number of slices:", imp.getNSlices()  
-  
+
+# Show the peaks as spheres in 3D, along with orthoslices:  
+univ = Image3DUniverse(512, 512)  
+# univ.addIcospheres(ps, Color3f(1, 0, 0), 2, cell_diameter/2, "Cells").setLocked(True)  
+# univ.addOrthoslice(imp).setLocked(True)  
+univ.show()
+
 # A list of green slices  
 greens = []  
   
@@ -27,4 +40,4 @@ for fp in greens:
 imp2 = ImagePlus("Green channel", stack2)  
 # Set a green look-up table:  
 IJ.run(imp2, "Green", "")  
-imp2.show() 
+imp2.show()
